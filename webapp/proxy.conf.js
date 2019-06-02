ec7829f6b88aae0885028175fb8f85347eda229e
daolao <dalao1002@gmail.com>
2019-06-02 17:24:10 +0200

    Revert "remove https"
    This reverts commit 139d3691

--- webapp/proxy.conf.js
+++ webapp/proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     "context": ["/api", "/logout", "/summary"], // TODO
-    "target": "http://localhost:8080",
+    "target": "https://localhost:8443",
     "secure": false,
     "changeOrigin": true,
     "logLevel": "debug"
